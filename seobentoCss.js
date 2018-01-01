e4801b3295da226189cb11d861a100fa4c3093f0
bento21 <bentobriker4@gmail.com>
2018-01-02 06:14:18 +0700

    Update seobentoCss.js

--- seobentoCss.js
+++ seobentoCss.js
@@ -8,7 +8,6 @@ $.fn.bentoCss = {
   resetResponsive : '',
 	response : function(){
 		$('head').append('<style type="text/css">' + this.layout + '</style>');
-		document.getElementsByTagName('head')[0].appendChild('<style type="text/css">' + this.layout + '</style>');
 	},  
 };$.fn.bentoCss.response();
   
